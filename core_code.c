9f112a976eb801b4199aad41a9f36ae4140034db
huangxiao2 <huangxiaoyang1201@gmail.com>
2019-08-31 09:20:38 +0800

    fix a bug about no_return and add a new line about the printf

--- core_code.c
+++ core_code.c
@@ -3,4 +3,6 @@
 int main(int num,char **args){
 printf("There are %d arguments flowing into the program.\n",num);
 printf("The first one is %s\n",args[0]);
+printf("The second one is %s\n",args[1]);
+return 0;
 }
