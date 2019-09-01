8d477cf7b1f93bb48730866dda25c4610a64fa9b
huangxiao2 <huangxiaoyang1201@gmail.com>
2019-09-01 13:22:47 +0800

    delete the file core_code.c and add a help for programing by C in bash

--- core_code.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(int num,char **args){
-printf("There are %d arguments flowing into the program.\n",num);
-printf("The first one is %s\n",args[0]);
-printf("The second one is %s\n",args[1]);
-return 0;
-}
